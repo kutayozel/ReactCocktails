edeca78f9624c815ab57544cc566c06a71ca0108
Kharza <zelkutay@gmail.com>
2021-07-26 17:23:34 +0300

    search

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dignissimos at commodi ducimus necessitatibus nemo voluptatum suscipit deserunt perferendis sapiente, repellendus obcaecati a dicta ipsum nihil qui minus itaque nam.</p>
+    </section>
   )
 }
 
